b6c66e7a799ea32c242610142423ee196cf46b70
chen-huai <52665471+chen-huai@users.noreply.github.com>
2023-11-22 14:14:31 +0800

    测试使用

--- Test.py
+++ Test.py
@@ -1,52 +1,52 @@
-class Test():
-    def __init__(self):
-        self.a = 1
-
-    def op1(func,self):
-        def warraper(*args, **kwargs):
-            self.a = 0
-            func(*args, **kwargs)
-        return warraper
-
-    def op2(func,self):
-        def warraper(*args, **kwargs):
-            self.a = 1
-            func(*args, **kwargs)
-        return warraper
-
-    @op1
-    def op3(self):
-        self.a += 1
-        print(3, self.a)
-
-    @op2
-    def op4(self):
-        self.a += 1
-        print(4, self.a)
-
-    @op1
-    def op5(self):
-        self.a += 1
-        print(5, self.a)
-
-    @op2
-    def op6(self):
-        self.a += 1
-        print(6, self.a)
-
-    def op7(self):
-        print(7, self.a)
-
-
-test = Test()
-test.op3()
-test.op7()
-test.op4()
-test.op7()
-test.op5()
-test.op7()
-test.op6()
-test.op7()
+# class Test():
+#     def __init__(self):
+#         self.a = 1
+#
+#     def op1(func,self):
+#         def warraper(*args, **kwargs):
+#             self.a = 0
+#             func(*args, **kwargs)
+#         return warraper
+#
+#     def op2(func,self):
+#         def warraper(*args, **kwargs):
+#             self.a = 1
+#             func(*args, **kwargs)
+#         return warraper
+#
+#     @op1
+#     def op3(self):
+#         self.a += 1
+#         print(3, self.a)
+#
+#     @op2
+#     def op4(self):
+#         self.a += 1
+#         print(4, self.a)
+#
+#     @op1
+#     def op5(self):
+#         self.a += 1
+#         print(5, self.a)
+#
+#     @op2
+#     def op6(self):
+#         self.a += 1
+#         print(6, self.a)
+#
+#     def op7(self):
+#         print(7, self.a)
+#
+#
+# test = Test()
+# test.op3()
+# test.op7()
+# test.op4()
+# test.op7()
+# test.op5()
+# test.op7()
+# test.op6()
+# test.op7()
 
 
 import time
@@ -115,3 +115,94 @@ import time
 #     # baiyu()  # 执行baiyu()就相当于执行wrapper()
 #
 #     baiyu()  # 用语法糖之后，就可以直接调用该函数了
+
+
+# # try测试是否可以包含没有错误的变量
+# class test():
+#     def __init__(self):
+#         self.a = ''
+#         self.b = ''
+#
+#     def test1(self):
+#         try:
+#             self.a = 1
+#             self.b = 2
+#             print(self.a)
+#             self.b = 'a' + '%s' % self.a
+#             print(self.b)
+#         except:
+#             self.a
+#             print(self.a)
+#
+#
+# test = test()
+# test.test1()
+
+
+# import random
+#
+# # 生成6个不重复的红球号码
+# red_balls = []
+# while len(red_balls) < 6:
+#     ball = random.randint(1, 33)
+#     if ball not in red_balls:
+#         red_balls.append(ball)
+#
+# # 生成1个蓝球号码
+# blue_ball = random.randint(1, 16)
+#
+# # 打印选号结果
+# print("红球号码：", sorted(red_balls))
+# print("蓝球号码：", blue_ball)
+
+# class PdfNameUpdater:
+#     def __init__(self):
+#         self.pdf_names = {'invoice': '', 'fapiao': ''}
+#
+#     def get_pdf_name(self, flag):
+#         return self.pdf_names.get(flag, '')
+#
+#     def update_pdf_name(self, msg, flag):
+#         if flag in self.pdf_names:
+#             pdf_name = self.pdf_names[flag]
+#             pdf_name_list = pdf_name.split(' + ')
+#
+#             if msg in pdf_name_list:
+#                 pdf_name_list.remove(msg)
+#             else:
+#                 pdf_name_list.append(msg)
+#
+#             changed_pdf_name = ' + '.join(pdf_name_list)
+#             self.pdf_names[flag] = changed_pdf_name
+#             return changed_pdf_name
+#         else:
+#             return ''
+#
+# # 使用示例
+# pdf_updater = PdfNameUpdater()
+# invoice_name = pdf_updater.get_pdf_name('invoice')
+# print("Invoice Name:", invoice_name)
+#
+# pdf_updater.update_pdf_name('Invoice1', 'invoice')
+# pdf_updater.update_pdf_name('Invoice2', 'invoice')
+# invoice_name = pdf_updater.get_pdf_name('invoice')
+# print("Updated Invoice Name:", invoice_name)
+from Get_Data import *
+from Test_Data import *
+import pandas as pd
+import os
+os.chdir('C:\\Users\\chen-fr\\Desktop\\临时文件\\sap')
+#读入数据
+#读入数据
+rawData = pd.read_excel('20230331.xlsx')
+
+# rawData['合并'] = rawData['Project No.'] + '\t' + rawData['Currency']
+# test = Test()
+# combineProject = rawData.groupby(["Invoices' name (Chinese)",'CS', 'Sales', 'Currency', 'Material Code', 'Buyer(GPC)', 'Month']).apply(test.concat_func).reset_index()
+
+rawData['row_msg'] = rawData['Project No.'] + '\t' + rawData['Currency']
+test = Get_Data()
+combineProject = rawData.groupby(["Invoices' name (Chinese)",'CS', 'Sales', 'Currency', 'Material Code', 'Buyer(GPC)', 'Month']).apply(test.row_concat_func).reset_index()
+
+result = pd.merge(rawData, combineProject, on=["Invoices' name (Chinese)",'CS', 'Sales', 'Currency', 'Material Code', 'Buyer(GPC)', 'Month'],how='right')
+print(result)
--- Test_Data.py
+++ Test_Data.py
@@ -1,27 +1,45 @@
 import pandas as pd
 import numpy as np
 import os
-os.chdir('C:\\Users\\chen-fr\\Desktop\\临时文件\\sap')
-#读入数据
-rawData = pd.read_excel('20230331.xlsx')
+# os.chdir('C:\\Users\\chen-fr\\Desktop\\临时文件\\sap')
+# #读入数据
+# #读入数据
+# rawData = pd.read_excel('20230331.xlsx')
 #定义拼接函数，并对字段进行去重
-def concat_func(x):
-    return pd.Series({
-        '合并':'\n'.join(x['合并'].unique()),
-        # '性别':','.join(x['性别'].unique())
-    }
-    )
-#分组聚合+拼接
-# result=rawData.groupby(rawData["Invoices' name (Chinese)"]).apply(concat_func).reset_index()
-# result = rawData.groupby(["Invoices' name (Chinese)",'CS', 'Sales', 'Currency', 'Material Code', 'Buyer(GPC)', 'Month']).sum(['Amount', 'Amount with VAT', 'Total Cost', 'Revenue\n(RMB)'])
+# def concat_func(x):
+#     return pd.Series({
+#         '合并': '\n'.join(x['合并'].unique()),
+#         # '性别':','.join(x['性别'].unique())
+#     }
+#     )
+# #分组聚合+拼接
+# # result=rawData.groupby(rawData["Invoices' name (Chinese)"]).apply(concat_func).reset_index()
+# # result = rawData.groupby(["Invoices' name (Chinese)",'CS', 'Sales', 'Currency', 'Material Code', 'Buyer(GPC)', 'Month']).sum(['Amount', 'Amount with VAT', 'Total Cost', 'Revenue\n(RMB)'])
+# # TODO
+#
+# # TODO 先合并每列数据，在判断组相同时合并每行数据,再添加列名;
+# rawData['合并']  = rawData['Project No.'] + '\t' + rawData['Currency']
+#
+# combineProject = rawData.groupby(["Invoices' name (Chinese)",'CS', 'Sales', 'Currency', 'Material Code', 'Buyer(GPC)', 'Month']).apply(concat_func).reset_index()
+#
+# result = pd.merge(rawData, combineProject, on=["Invoices' name (Chinese)",'CS', 'Sales', 'Currency', 'Material Code', 'Buyer(GPC)', 'Month'],how='right')
+#
+# # result['Test'] =  'Payment Notice\nPO' + '\n' + result['合并']
+#
+# #结果展示
+# result.to_csv('result.csv', encoding='utf_8_sig')
+class Test():
+    def concat_func(self,x):
+        return pd.Series({
+            '合并': '\n'.join(x['合并'].unique()),
+            # '性别':','.join(x['性别'].unique())
+        }
+        )
 
-rawData['合并']  = rawData['Project No.'] + '\t' + rawData['Currency']
-
-combineProject = rawData.groupby(["Invoices' name (Chinese)",'CS', 'Sales', 'Currency', 'Material Code', 'Buyer(GPC)', 'Month']).apply(concat_func).reset_index()
-
-result = pd.merge(rawData, combineProject, on=["Invoices' name (Chinese)",'CS', 'Sales', 'Currency', 'Material Code', 'Buyer(GPC)', 'Month'],how='right')
-
-# result['Test'] =  'Payment Notice\nPO' + '\n' + result['合并']
-
-#结果展示
-result.to_csv('result.csv', encoding='utf_8_sig')
+# rawData['合并']  = rawData['Project No.'] + '\t' + rawData['Currency']
+#
+# test = Test()
+# combineProject = rawData.groupby(["Invoices' name (Chinese)",'CS', 'Sales', 'Currency', 'Material Code', 'Buyer(GPC)', 'Month']).apply(test.concat_func).reset_index()
+#
+# result = pd.merge(rawData, combineProject, on=["Invoices' name (Chinese)",'CS', 'Sales', 'Currency', 'Material Code', 'Buyer(GPC)', 'Month'],how='right')
+# print(result)
